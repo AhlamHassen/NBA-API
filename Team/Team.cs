e11cdd574230848a1b212d635f3adc3d69334e74
AhlamHassen <ahlamsaleh199700@gmail.com>
2021-03-23 22:56:43 +1100

    Defined class Team& completed the checkTeam method

--- Team/Team.cs
+++ Team/Team.cs
@@ -1,8 +1,45 @@
 ﻿using System;
+using Newtonsoft.Json;
+using System.Data.SqlClient;
+using System.Collections.Generic;
 
 namespace Team
 {
     public class Team
     {
+        [JsonProperty("TeamName")]
+        public string TeamName { get; set; }
+
+        public Team(){
+            this.TeamName = " ";
+        }
+
+        public Team(string tname){
+            this.TeamName = tname;
+        }
+
+        public Team checkTeam(Team t){
+            string connectionString = @"Data Source=databaseSourceHere;
+            Initial Catalog=databaseNameHere;User ID=admin/TheIDUsed; Password=yourpassonAWS";
+            SqlConnection con = new SqlConnection(connectionString);
+
+            string queryString = "SELECT * FROM Team WHERE TeamName = @team";
+
+            SqlCommand command = new SqlCommand(queryString, con);
+            command.Parameters.AddWithValue("@team", t.TeamName);
+            con.Open();
+            Team found = null;
+
+            using(SqlDataReader reader = command.ExecuteReader()){
+                while(reader.Read())
+                {
+                   found = new Team(reader[0].ToString());
+                }
+            }
+
+            return found;
+        }
+
+        
     }
 }

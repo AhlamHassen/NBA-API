ad478e7cf29d0cd7ad26bd487d03a31e956c3b5a
AhlamHassen <ahlamsaleh199700@gmail.com>
2021-03-23 22:50:15 +1100

    Created webapi, sln and classLib, added all to sln

---
+++ Team/Team.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Team
+{
+    public class Team
+    {
+    }
+}
---
+++ webApi/Controllers/NbaApiController.cs
@@ -0,0 +1,10 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace webApi.Controllers
+{
+    public class NbaApiController
+    {
+        
+    }
+}
